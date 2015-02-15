6a6ede8483f84f842da787d03a14e3614916fff3
Eithel <tux.eithel@gmail.com>
2015-02-15 20:16:25 +0100

    First basic introduction of flags. First skeleton for the function who in the future will manage connections

--- gosiege.go
+++ gosiege.go
@@ -1,9 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net/url"
+	"time"
+
+	"bitbucket.org/tux-eithel/gosiege/parseUrl"
 )
 
+// -n param
+var numberConcurrent int
+
+// -s param
+var secToWait time.Duration
+
+// -u param
+var listUrls parseUrl.Urls
+
+func init() {
+	flag.IntVar(&numberConcurrent, "n", 1, "Number of concurrent request")
+	flag.DurationVar(&secToWait, "s", time.Duration(1)*time.Second, "Time to wait until next request")
+	flag.Var(&listUrls, "u", "Url(s) to test")
+}
+
 func main() {
-	fmt.Println("hello!")
+	flag.Parse()
+	//	fmt.Println(numberConcurrent)
+	//	fmt.Println(secToWait)
+	//	fmt.Println(listUrls.String())
+
+	for i := 0; i < len(listUrls); i++ {
+		ToRun(&listUrls)
+	}
+}
+
+func ToRun(totest parseUrl.RandomUri) error {
+
+	var uri *url.URL
+	var srt *string
+	var err error
+
+	//	for {
+	srt, err = totest.GetRandomUri()
+	if err != nil {
+		return err
+	}
+	uri, err = url.Parse(*srt)
+	fmt.Printf("%#v\n", uri)
+	// do Stuff
+	time.Sleep(secToWait)
+	//	}
+
+	return nil
 }
