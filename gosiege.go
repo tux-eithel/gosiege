7f8afa307622436b3ff3b51036cfc686f15a9480
Eithel <tux.eithel@gmail.com>
2015-02-18 15:17:05 +0100

    Added two new flags:  * -nasty for use the all CPU cores (default false)  * -r select random urls from list (default true)
    Now we can stop the requests using ctr+c and wait that all request terminate.
    Also function NextUri is SafeThread

--- gosiege.go
+++ gosiege.go
@@ -3,7 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	_ "log"
 	_ "net/url"
+	"os"
+	"os/signal"
+	"runtime"
+	"sync"
+	"syscall"
 	"time"
 
 	"bitbucket.org/tux-eithel/gosiege/parseUrl"
@@ -21,6 +27,12 @@ var listUrls parseUrl.FlagUrl
 // -f param
 var inputFile string
 
+// -nasty param
+var isNasty bool
+
+// r param
+var randomUrl bool
+
 func init() {
 
 	listUrls.Init()
@@ -28,31 +40,80 @@ func init() {
 	flag.DurationVar(&secToWait, "s", time.Duration(1)*time.Second, "Time to wait until next request")
 	flag.Var(&listUrls, "u", "Url(s) to test")
 	flag.StringVar(&inputFile, "f", "", "Input file with urls")
+	flag.BoolVar(&isNasty, "nasty", false, "Use all available CPU cores")
+	flag.BoolVar(&randomUrl, "r", true, "Use random urls from list")
 }
 
 func main() {
 	flag.Parse()
+
+	if isNasty {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
 	//	fmt.Println(numberConcurrent)
 	//	fmt.Println(secToWait)
 	//	fmt.Println(listUrls.String())
 
-	for i := 0; i < len(listUrls.Req.Reqs); i++ {
-		ToRun(listUrls.Req)
+	quitChannel := make(chan os.Signal)
+	signal.Notify(quitChannel, syscall.SIGINT, syscall.SIGTERM)
+
+	shutdownChannel := make(chan bool)
+
+	nextChannel := make(chan int)
+
+	waitGroup := &sync.WaitGroup{}
+
+	waitGroup.Add(1)
+
+	for i := 0; i < numberConcurrent; i++ {
+		go ToRun(listUrls.Req, randomUrl, nextChannel, shutdownChannel, waitGroup)
 	}
+
+	if !randomUrl {
+		nextChannel <- 1
+	}
+
+	<-quitChannel
+	shutdownChannel <- true
+	fmt.Println("Received quit. Sending shutdown and waiting on goroutines...")
+
+	/*
+	 * Block until wait group counter gets to zero
+	 */
+	waitGroup.Wait()
+	fmt.Println("Done.")
 }
 
-func ToRun(totest *parseUrl.Requests) error {
+func ToRun(totest *parseUrl.Requests, randomUrl bool, nextChannel chan int, shutdownChannel chan bool, waitGroup *sync.WaitGroup) error {
 
-	//	for {
-	req := totest.NextUri(false)
-	if req == nil {
-		fmt.Println("Qualcosa di sbagliato")
+	defer waitGroup.Done()
+	for {
+
+		select {
+		case _ = <-shutdownChannel:
+			return nil
+
+		default:
+		}
+
+		if !randomUrl {
+			<-nextChannel
+		}
+		req := totest.NextUri(randomUrl)
+		if !randomUrl {
+			nextChannel <- 1
+		}
+
+		if req == nil {
+			fmt.Println("Qualcosa di sbagliato")
+		}
+
+		fmt.Printf("%#v\n", req.Url)
+		// do Stuff
+		time.Sleep(secToWait)
+
 	}
-
-	fmt.Printf("%#v\n", req)
-	// do Stuff
-	time.Sleep(secToWait)
-	//	}
 
 	return nil
 }
