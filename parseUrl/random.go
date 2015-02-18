0c04abb5f243c371af2d70338d2f5db386244ae9
Eithel <tux.eithel@gmail.com>
2015-02-18 12:15:17 +0100

    Removed interface, we maybe don't need anymore

--- parseUrl/random.go
+++
@@ -1,7 +0,0 @@
-// parseUrl let you to parse urls from command line or a file (passed via command line)
-package parseUrl
-
-// RandomUri is an interface for retrive a random string form a object
-type RandomUri interface {
-	GetRandomUri() (*string, error)
-}
