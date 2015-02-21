9d47672b8571c56c1ae1d4907bea679efb6a2ea3
Eithel <tux.eithel@gmail.com>
2015-02-21 16:22:42 +0100

    Now we don't check the url. NewInputRequest will do

--- parseUrl/flag.go
+++ parseUrl/flag.go
@@ -2,7 +2,6 @@ package parseUrl
 
 import (
 	"fmt"
-	"net/url"
 	"strings"
 )
 
@@ -26,11 +25,15 @@ func (fu *FlagUrl) Set(srt string) error {
 	fu.Req = NewRequests()
 	app := strings.Split(srt, " ")
 	for _, value := range app {
-		_, err := url.Parse(value)
+
+		appR, err := NewInputRequest(value)
 		if err != nil {
-			fmt.Println("Url: ", value, "not correct, skipped")
+			fmt.Println("Error making Request for '", value, "' with error:", err)
+			fmt.Println("Url ignored")
+		} else {
+			fu.Req.AddRequest(appR)
 		}
-		fu.Req.AddRequest(NewInputRequest(value))
+
 	}
 	return nil
 }
