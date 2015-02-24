5b44dfd4d9287b8451e960e27915966f8abe303f
Eithel <tux.eithel@gmail.com>
2015-02-24 12:54:02 +0100

    Now we have made NextUri thread-safe, so in main function is not needed anymore

--- libgosiege/request.go
+++ libgosiege/request.go
@@ -59,17 +59,21 @@ func NewInputRequest(inputUrl string) (*InputRequest, error) {
 // Its structure also contains the current-1 object to extract in case of sequential reading,
 // or an object for random number generation in case of random reading
 type Requests struct {
-	Reqs []*InputRequest
-	Rand *rand.Rand
-	Cont int
+	Reqs        []*InputRequest
+	Rand        *rand.Rand
+	Cont        int
+	NextChannel chan int
 }
 
 // NewRequests inizialize a Requests object
 func NewRequests() *Requests {
-	return &Requests{
-		Rand: rand.New(rand.NewSource(time.Now().UnixNano())),
-		Cont: -1,
+	appR := &Requests{
+		Rand:        rand.New(rand.NewSource(time.Now().UnixNano())),
+		Cont:        -1,
+		NextChannel: make(chan int, 1),
 	}
+	appR.NextChannel <- 1
+	return appR
 }
 
 // AddRequest add to a Requests an InputRequest
@@ -80,16 +84,29 @@ func (r *Requests) AddRequest(ir *InputRequest) {
 
 // NextUri return the next uri to be processed. If isRandom is true, a random url from input is returned, else the next one in order.
 // Pass isRandom as false value is useful if you want to process all the input urls
+// Now NextUri is thread-safe when isRandom is false. Well is thread-safe the sum of index for next element, but not the result of the function!
+// For that, you have to make thread-safe this function when you are going to call it. But if you really want this it's better run gosiege with -n 1 param
 func (r *Requests) NextUri(isRandom bool) *InputRequest {
+
+	if len(r.Reqs) == 0 {
+		return nil
+	}
+
+	if len(r.Reqs) == 1 {
+		return r.Reqs[0]
+	}
+
 	if isRandom {
 		return r.Reqs[r.Rand.Intn(len(r.Reqs))]
 	}
 
+	<-r.NextChannel
 	if r.Cont+1 == len(r.Reqs) {
 		r.Cont = 0
 	} else {
 		r.Cont++
 	}
+	r.NextChannel <- 1
 	return r.Reqs[r.Cont]
 }
 
